558d223a4e859724b22229a6b2070ba00150ffae
jkolp <jkolp.dev@gmail.com>
2020-06-23 22:52:10 -0700

    Finished

---
+++ i_am_rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  i_am_rich
+//
+//  Created by Projects on 6/23/20.
+//  Copyright © 2020 Jen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
